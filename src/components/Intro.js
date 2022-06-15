944a0a8db989ccfa7f14b04172bf1036d4c7728b
kritik <kritikverma2002@gmail.com>
2022-06-15 20:18:23 +0530

    under maintenance

--- src/components/Intro.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Intro() {
-  return <div>this is my intro</div>;
-}
-
-export default Intro;
--- src/pages/maintenance/Maintenance.js
+++ src/pages/maintenance/Maintenance.js
@@ -8,7 +8,7 @@ function Maintenance() {
       <div className="logo">
         <h1>KV</h1>
       </div>
-      <div>
+      <div className="content">
         <img src={logo} alt="" />
         <h1>Under Maintenance!</h1>
         <p>To make things right we need some time to rebuild</p>
